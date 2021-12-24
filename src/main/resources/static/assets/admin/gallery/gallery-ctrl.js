dc3f83ac5222df9e37fe2feaaca67fe8208c7e71
khuong <levankhuong1403@gmail.com>
2021-12-24 23:46:55 +0700

    commit3

--- src/main/resources/static/assets/admin/gallery/gallery-ctrl.js
+++ src/main/resources/static/assets/admin/gallery/gallery-ctrl.js
@@ -13,8 +13,8 @@ app.controller("gallery-ctrl", function($scope, $http){
 	$scope.list();
 	
 	$scope.delete = function(filename){
-		$http.delete("/rest/upload/images/${filename}").then(resp => {
-			var i = $scope.filenames.findIndex(name => name == filename);
+		$http.delete(`/rest/upload/images/${filename}`).then(resp => {
+			let i = $scope.filenames.findIndex(name => name == filename);
 			$scope.filenames.splice(i, 1);
 			alert("Xóa ảnh thành công !");
 		}).catch(error => {
